af9de7fd0f0f1a7b9ad10f8c8551a7547361a797
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-09 19:03:55 +0530

    added axios

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import {users} from '../db.json'
 import { Table,Button } from 'react-bootstrap'
+import axios from 'axios'
 const Home = () => {
+  const[data,setData]=useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/users')
+    .then(res=>setData(res.data))
+    .catch(err=>console.log(err))
+
+  
+  },[])
   return (
     <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
       <h1>List of Users</h1>
       <div className='w-75 rounded bg-white border shadow p-4'>
 
-      <Table>
+      <Table className='table table-striped'>
       <thead>
         <tr>
           <th>
@@ -28,17 +38,38 @@ const Home = () => {
           <th>
            Website
           </th>
+          <th>
+           Action
+          </th>
         </tr>
       </thead>
-      {users.map((items)=>
+      {data.map((items,i)=>
     <tbody>
-    <tr>
-      <th>
+    <tr key={i}>
+      <td>
 {items.id}
-      </th>
-      <th>
+      </td>
+      <td>
 {items.name}
-      </th>
+      </td>
+      <td>
+{items.username}
+      </td>
+      <td>
+{items.email}
+      </td>
+      <td>
+{items.phone}
+      </td>
+      <td>
+{items.website}
+      </td>
+      <td>
+        
+      <Button className='btn btn-sm btn-info me-2' >Edit</Button>
+      <Button className='btn btn-sm btn-primary me-2' >Read</Button>
+      <Button className='btn btn-sm btn-danger me-2' >Delete</Button>
+      </td>
     </tr>
   </tbody>)}
       
