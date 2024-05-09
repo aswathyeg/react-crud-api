f54100d403a88274728362de6e1882bfce47aaf0
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-09 12:23:00 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 import './App.css'
-
+import Home from './Home'
+import Create from './Create'
+import Read from './Read'
+import Update from './Update'
 function App() {
   
 
@@ -10,9 +13,18 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/'
-      element={<Home/>}>
+      element={<Home/>}/>
+       
 
-      </Route>
+        <Route path='/create'
+      element={<Create/>}/>
+        
+        <Route path='/read/:id'
+      element={<Read/>}/>
+       <Route path='/update/:id'
+      element={<Update/>}/>
+       
+           
     </Routes>
     </BrowserRouter>
    
